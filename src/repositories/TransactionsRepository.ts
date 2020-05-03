b47dcb19c23fdc4add1b49e648d199e98a5594cf
João Gabriel <joao.royosilva@gmail.com>
2020-05-02 21:58:50 -0400

    finalização

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (balance: Balance, transaction: Transaction): Balance => {
+        switch (transaction.type) {
+          case 'income':
+            balance.income += transaction.value;
+            balance.total += transaction.value;
+            break;
+          case 'outcome':
+            balance.outcome += transaction.value;
+            balance.total -= transaction.value;
+            break;
+          default:
+            break;
+        }
+        return balance;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
